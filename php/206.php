786eed5e28b78c9bfceae84d9f9befde9f0b09dc
Tom Rochette <roctom@gmail.com>
2016-04-07 15:57:05 -0400

    Add solution to problem 206.

---
+++ php/206.php
@@ -0,0 +1,29 @@
+<?php
+
+// There are 378 925 620 squares that can be formed from 1020304050607080900 to 1929394959697989990.
+// Testing all possible combinations would require 10^9 = 1 000 000 000, an order more tests required.
+// Since it ends with a 0, we know the square must be a multiple of 10, which cuts our number of squares to test by 10.
+function q206()
+{
+	$min = 1020304050607080900;
+	$max = 1929394959697989990;
+	$minSqrt = (int)floor(sqrt($min) / 10) * 10;
+	$maxSqrt = (int)ceil(sqrt($max) / 10) * 10;
+	for ($i = $minSqrt; $i < $maxSqrt; $i += 10) {
+		$square = $i * $i;
+		$square = (string)$square;
+		$allMatches = true;
+		for ($j = 1; $j <= 9; ++$j) {
+			if ((int)$square[($j-1)*2] !== $j) {
+				$allMatches = false;
+				break;
+			}
+		}
+
+		if ($allMatches) {
+			return $i;
+		}
+	}
+}
+
+echo q206();
