e564bacd17a481f7d3e37b40d41acc94f0c00223
Tom Rochette <roctom@gmail.com>
2016-04-11 23:46:18 -0400

    Optimize problem 9 using the prime generator.

--- php/7.php
+++ php/7.php
@@ -1,39 +1,19 @@
 <?php
 
+use tomzx\ProjectEuler\Generator\Prime;
+
+require_once 'vendor/autoload.php';
+
 // Find the $number-th prime number
 function q7($number) {
-	$primes = [2];
-	$currentNumber = 2;
-	while (true) {
-		++$currentNumber;
-		if ($currentNumber & 2 === 0) {
-			continue;
-		}
-
-		$squareRoot = sqrt($currentNumber);
-
-		$foundPrimeDivisor = false;
-		foreach ($primes as  $prime) {
-			if ($prime > $squareRoot) {
-				break;
-			}
-
-			if ($currentNumber % $prime === 0) {
-				$foundPrimeDivisor = true;
-				break;
-			}
-		}
-
-		if ( ! $foundPrimeDivisor) {
-			$primes[] = $currentNumber;
-//			echo 'added '.$currentNumber.PHP_EOL;
-		}
-
-		if (count($primes) === $number) {
-			return $primes[$number - 1];
+	$count = 0;
+	foreach (Prime::generator() as $prime) {
+		++$count;
+		if ($count === $number) {
+			return $prime;
 		}
 	}
 }
 
-//echo q7(6);
+assert(13 === q7(6));
 echo q7(10001);
