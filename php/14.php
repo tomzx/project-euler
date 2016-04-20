35977b3bf46b847bf2870bd54e0f290263aabc89
Tom Rochette <roctom@gmail.com>
2016-04-20 00:48:30 -0400

    Optimize problem 14. - Add a cache variable to store precomputed collatz sequence solutions

--- php/14.php
+++ php/14.php
@@ -3,27 +3,34 @@
 // Note: make sure to run this on x64 for large numbers
 function q14($max)
 {
+	$cache = [];
 	$currentMax = 0;
 	$startingNumber = 1;
 	for ($i = 1; $i <= $max; ++$i) {
 		$length = 1;
 		$number = $i;
 		while ($number !== 1) {
+			if (isset($cache[$number])) {
+				$length += $cache[$number] - 1;
+				break;
+			}
+
 			++$length;
 			if (($number & 1) === 0) {
 				$number /= 2;
 			} else {
 				$number = 3 * $number + 1;
 			}
-//			echo $number.PHP_EOL;
 		}
+
+		$cache[$i] = $length;
+
 		if ($length > $currentMax) {
 			$currentMax = $length;
 			$startingNumber = $i;
 		}
-//		echo $i.': '.$currentMax.PHP_EOL;
 	}
-	return [$currentMax, $startingNumber];
+	return $startingNumber;
 }
 
-var_dump(q14(1000000));
+echo(q14(1000000));
